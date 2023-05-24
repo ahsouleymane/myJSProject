4fce2bca85cd065b1fa1bfc7f92ab1dd96a923ad
Souleymane Abdou <souleymaneah98@gmail.com>
2023-05-24 18:00:06 +0100

    classes

---
+++ classes/class_in_js.js
@@ -0,0 +1,22 @@
+/**
+ * Class is introduced in JS in ECMAScript 6 (ES6) release
+ */
+
+function Employee(id, name, email){
+    this.id = id
+    this.name = name
+    this.email = email
+}
+
+class User {
+    constructor(id, name){
+        this.id = id
+        this.name = name
+    }
+}
+
+let a = new Employee(12456, "Souleymane", "soul45@gmail.com")
+let b = new User(321045, "Abdou")
+
+console.log(a)
+console.log(b)
